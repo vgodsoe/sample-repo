418e48d51a09eba9c8ddb30d9833328fbe0afdc5
Victoria Godsoe <vgodsoe@gmail.com>
2024-06-05 11:27:42 -0700

    Update filter_sensitive_files.sh

--- filter_sensitive_files.sh
+++ filter_sensitive_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure script is run from the repository root
-cd "$(dirname "$0")/../.."
+cd "$(dirname "$0")"
 
 # Filter sensitive files with --force option
 git filter-repo --force --path test_private.json --invert-paths
